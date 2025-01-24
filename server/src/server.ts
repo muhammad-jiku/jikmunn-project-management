c150b9cf85e971fa2101ecfb4ab9c0ca3410fc00
muhammad-jiku <muhammadjiku364@gmail.com>
2025-01-24 12:05:40 +0600

    shared dir and logger init

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,29 @@
 import { Server } from 'http';
 import app from './app';
 import config from './config';
+import { errorlogger, logger } from './shared/logger';
 
 process.on('uncaughtException', (error) => {
-  console.error(error);
+  errorlogger.error(error);
   process.exit(1);
 });
 
 async function bootstrap() {
   const server: Server = app.listen(config.port, () => {
-    console.log(`Server running on http://localhost:${config.port}`);
+    logger.info(`Server running on http://localhost:${config.port}`);
   });
 
   const exitHandler = () => {
     if (server) {
       server.close(() => {
-        console.log('Server closed');
+        logger.info('Server closed');
       });
     }
     process.exit(1);
   };
 
   const unexpectedErrorHandler = (error: unknown) => {
-    console.error(error);
+    errorlogger.error(error);
     exitHandler();
   };
 
@@ -30,7 +31,7 @@ async function bootstrap() {
   process.on('unhandledRejection', unexpectedErrorHandler);
 
   process.on('SIGTERM', () => {
-    console.log('SIGTERM received');
+    logger.info('SIGTERM received');
     if (server) {
       server.close();
     }
