db647f8b828e9360d7d6349c46bf0641f8d69530
muhammad-jiku <muhammadjiku364@gmail.com>
2025-01-24 12:27:43 +0600

    errors dir added

---
+++ server/src/errors/handleValidationError.ts
@@ -0,0 +1,23 @@
+import { Prisma } from '@prisma/client';
+import { IGenericErrorResponse } from '../interfaces/error';
+
+const handleValidationError = (
+  err: Prisma.PrismaClientValidationError
+): IGenericErrorResponse => {
+  const errors = [
+    {
+      path: '',
+      message: err.message,
+    },
+  ];
+
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'Validation error!',
+    errorMessages: errors,
+  };
+};
+
+export default handleValidationError;
