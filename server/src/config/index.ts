f7e79956dec79c59b799398e7ae5d1513dc266de
muhammad-jiku <muhammadjiku364@gmail.com>
2025-02-05 21:36:06 +0600

    config file modified

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -7,4 +7,17 @@ export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
+  default: {
+    developer_pass: process.env.DEFAULT_DEVELOPER_PASS,
+    manager_pass: process.env.DEFAULT_MANAGER_PASS,
+    admin_pass: process.env.DEFAULT_ADMIN_PASS,
+    super_admin_pass: process.env.DEFAULT_SUPER_ADMIN_PASS,
+  },
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expires_in: process.env.JWT_EXPIRES_IN,
+    refresh_secret: process.env.JWT_REFRESH_SECRET,
+    refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  },
 };
