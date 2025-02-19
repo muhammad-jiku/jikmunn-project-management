b61eecf0e0976d2d3e61d7d0215268f173df988a
muhammad-jiku <muhammadjiku364@gmail.com>
2025-02-19 19:17:42 +0600

    timeline page data fetching added

--- client/src/app/timeline/page.tsx
+++ client/src/app/timeline/page.tsx
@@ -1,38 +1,39 @@
 'use client';
 
 import Header from '@/_components/Header';
-// import { useGetProjectsQuery } from '@/state/api';
-// import { useAppSelector } from '@/store';
-import { DisplayOption, ViewMode } from 'gantt-task-react';
+import { useGetProjectsQuery } from '@/state/api/projectsApi';
+import { useAppSelector } from '@/store';
+import { DisplayOption, Gantt, ViewMode } from 'gantt-task-react';
 import 'gantt-task-react/dist/index.css';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
-// type TaskTypeItems = 'task' | 'milestone' | 'project';
+type TaskTypeItems = 'task' | 'milestone' | 'project';
 
 const Timeline = () => {
-  // const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
-  // const { data, isLoading, isError } = useGetProjectsQuery({});
-  // console.log('timeline page projects data', data);
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
   const [displayOptions, setDisplayOptions] = useState<DisplayOption>({
     viewMode: ViewMode.Month,
     locale: 'en-US',
   });
 
-  // const ganttTasks = useMemo(() => {
-  //   return (
-  //     data?.data?.map((project) => ({
-  //       start: new Date(project.startDate as string),
-  //       end: new Date(project.endDate as string),
-  //       name: project.name,
-  //       id: `Project-${project.id}`,
-  //       type: 'project' as TaskTypeItems,
-  //       progress: 50,
-  //       isDisabled: false,
-  //     })) || []
-  //   );
-  // }, [data]);
+  const { data: projects, isLoading, isError } = useGetProjectsQuery({});
+  console.log('timeline page projects data', projects);
 
-  // console.log('timeline page ganttasks', ganttTasks);
+  const ganttTasks = useMemo(() => {
+    return (
+      projects?.data?.map((project) => ({
+        start: new Date(project.startDate as string),
+        end: new Date(project.endDate as string),
+        name: project.title,
+        id: `Project-${project.id}`,
+        type: 'project' as TaskTypeItems,
+        progress: 50,
+        isDisabled: false,
+      })) || []
+    );
+  }, [projects]);
+
+  console.log('timeline page ganttasks', ganttTasks);
 
   const handleViewModeChange = (
     event: React.ChangeEvent<HTMLSelectElement>
@@ -43,9 +44,9 @@ const Timeline = () => {
     }));
   };
 
-  // if (isLoading) return <div>Loading...</div>;
-  // if (isError || !data)
-  //   return <div>An error occurred while fetching projects</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (isError || !projects)
+    return <div>An error occurred while fetching projects</div>;
 
   return (
     <div className='max-w-full p-8'>
@@ -66,7 +67,7 @@ const Timeline = () => {
 
       <div className='overflow-hidden rounded-md bg-white shadow dark:bg-dark-secondary dark:text-white'>
         <div className='timeline'>
-          {/* <Gantt
+          <Gantt
             tasks={ganttTasks}
             {...displayOptions}
             columnWidth={displayOptions.viewMode === ViewMode.Month ? 150 : 100}
@@ -74,7 +75,7 @@ const Timeline = () => {
             projectBackgroundColor={isDarkMode ? '#101214' : '#1f2937'}
             projectProgressColor={isDarkMode ? '#1f2937' : '#aeb8c2'}
             projectProgressSelectedColor={isDarkMode ? '#000' : '#9ba1a6'}
-          /> */}
+          />
         </div>
       </div>
     </div>
